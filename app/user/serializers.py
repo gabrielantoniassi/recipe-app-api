c16c4a3a62f93298ac580481572f4c87bd626f58
Gabriel Antoniassi <gabrielantoniassi12@gmail.com>
2023-01-23 20:32:16 -0300

    Implement manage user API

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -22,6 +22,17 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password."""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
@@ -43,7 +54,6 @@ class AuthTokenSerializer(serializers.Serializer):
         if not user:
             msg = _('Unable to authenticate with provided credentials.')
             raise serializers.ValidationError(msg, code='authorization')
-
 
         attrs['user'] = user
         return attrs
